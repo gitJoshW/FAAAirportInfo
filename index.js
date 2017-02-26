b1a231a401f2f5a1badb487b606ec0040033bb4d
gitJoshW <jdwaldman@gmail.com>
2017-02-26 15:44:31 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+'use strict';
+module.change_code = 1;
+var _ = require('lodash');
+var Alexa = require('alexa-app');
+var app = new Alexa.app('airportinfo');
+var FAADataHelper = require('./faa_data_helper');
+
+//app.launch is automatically triggered any time the skill is invoked
+// the reprompt will trigger ~8 seconds after the first prompt if that wasn't answered
+app.launch(function(req, res) {
+  var prompt = 'For delay information, tell me an Airport code.';
+  res.say(prompt).reprompt(prompt).shouldEndSession(false);
+});
+app.intent('airportinfo', {
+  'slots': {
+    'AIRPORTCODE': 'FAACODES'
+  },
+  'utterances': ['{|flight|airport} {|delay|status} {|info} {|for} {-|AIRPORTCODE}']
+},
+  function(req, res) {
+    //get the slot
+    var airportCode = req.slot('AIRPORTCODE');
+    var reprompt = 'Tell me an airport code to get delay information.';
+    if(_.isEmpty(airportCode)) {
+      var prompt = 'I didn\'t hear an airport code. Tell me an airport code.';
+      res.say(prompt).reprompt(reprompt).shouldEndSession(false);
+      return true;
+    } else {
+      var faaHelper = new FAADataHelper();
+      faaHelper.requestAirportStatus(airportCode).then(function(airportStatus) {
+        console.log(airportStatus);
+        res.say(faaHelper.formatAirportStatus(airportStatus)).send();
+      }).catch(function(err) {
+        console.log(err.statusCode);
+        var prompt = 'i didn\'t have data for an airport code of ' + airportCode;
+        res.say(prompt).reprompt(reprompt).shouldEndSession(false).send();
+      });
+      return false;
+    }
+
+  }
+);
+module.exports = app;
